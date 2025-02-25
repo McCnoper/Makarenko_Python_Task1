84250e792966a031cc0419c9f1bc0c75c9fb86c5
McCnoper <r00mmmaaaa@gmail.com>
2025-02-25 22:56:19 +0200

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,8 @@
+print("Hello, Python!");
+name = input("Enter your name: ")
+print("Hello "+name);
+x = input("Enter first number: ")
+y = input("Enter Second number: ")
+
+print(int(x) + int(y))
+ 
